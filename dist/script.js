a3cba9f2b2f7d7e495f4f3af3e5e99c7cdf4084f
Rishi <85361929+rish4863@users.noreply.github.com>
2022-11-07 22:33:30 -0600

    Delete script.js

--- dist/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $("#greetingOutput").text("");
-
-  $("#sendName").click(function () {
-    //console.log($("#fname").val());
-    $("#greetingOutput").text("Hello " + $("#fname").val() +" "+ $("#lname").val() +" Your email address  and your birthdate are: "+ $ ("#emailaddress").val()+  " "+ $("#dateofbirth").val() );
-    
-  });
-});
