af6aaa08e1307990a8540f33bac94ea9eb8f323b
Rishi <85361929+rish4863@users.noreply.github.com>
2022-11-07 22:14:18 -0600

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $("#greetingOutput").text("");
+
+  $("#sendName").click(function () {
+    //console.log($("#fname").val());
+    $("#greetingOutput").text("Hello " + $("#fname").val() +" "+ $("#lname").val() +" Your email address  and your birthdate are: "+ $ ("#emailaddress").val()+  " "+ $("#dateofbirth").val() );
+    
+  });
+});
